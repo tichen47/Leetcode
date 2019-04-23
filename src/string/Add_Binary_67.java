8e672a54ffbc6c4c60fb55bb50a9e7959523816b
tichen47 <tichen47@gmail.com>
2019-04-22 22:52:22 -0500

    Problem 67

---
+++ src/string/Add_Binary_67.java
@@ -0,0 +1,27 @@
+package string;
+
+public class Add_Binary_67 {
+    
+    /*
+     * Solution: Straight Forward 
+     * Time Complexity: O(2*(M+N))
+     * Space Complexity: O(1?N)
+     */
+    public String addBinary(String a, String b) {
+        StringBuilder sb = new StringBuilder();
+        int i = a.length() - 1;
+        int j = b.length() - 1;
+        int carry = 0;
+        int sum;
+        while(i >= 0 || j >= 0){
+            sum = carry;
+            if(i>=0) sum += a.charAt(i--) - '0';
+            if(j>=0) sum += b.charAt(j--) - '0';
+            sb.append(sum % 2);
+            carry = sum / 2;
+        }
+        if(carry > 0)
+            sb.append(carry);
+        return sb.reverse().toString();
+    }
+}
