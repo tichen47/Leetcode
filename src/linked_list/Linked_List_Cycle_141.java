37d5c37cd1d9198070e0c20b795ea41c6d6311cd
tichen47 <tichen47@gmail.com>
2019-04-23 23:56:42 -0500

    Problem 141

--- src/linked_list/Linked_List_Cycle_141.java
+++ src/linked_list/Linked_List_Cycle_141.java
@@ -18,4 +18,21 @@ public class Linked_List_Cycle_141 {
         }
         return false;
     }
+    
+    /*
+     * Solution: Two Pointer
+     * Time Complexity: O(N)
+     * Space Complexity: O(1)
+     */
+    public boolean hasCycle2(ListNode head) {
+        if(head == null) return false;
+        ListNode fast = head;
+        ListNode slow = head;
+        while(fast.next != null && fast.next.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
+            if(fast == slow) return true;
+        }
+        return false;
+    }
 }
