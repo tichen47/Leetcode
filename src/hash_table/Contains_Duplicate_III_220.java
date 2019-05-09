26697fe652a4c6334bf1db0e9ba6b7677712e7c1
tichen47 <tichen47@gmail.com>
2019-05-09 16:41:21 -0500

    Problem 220

---
+++ src/hash_table/Contains_Duplicate_III_220.java
@@ -0,0 +1,26 @@
+package hash_table;
+
+import java.util.*;
+
+public class Contains_Duplicate_III_220 {
+    
+    /*
+     * Sliding Window
+     * Time Complexity: O(N) 
+     * Space Complexity: O(k)
+     */
+    public boolean containsNearbyAlmostDuplicate(int[] nums, int k, int t) {
+        if(k <= 0) return false;
+        TreeSet<Long> set = new TreeSet<>();
+        int l = 0, r = 0;
+        while(r < nums.length - 1){
+            if(r - l >= k) { 
+                set.remove((long)nums[l++]);
+            }
+            set.add((long)nums[r++]);
+            if(set.ceiling((long)nums[r]-(long)t) != null && set.ceiling((long)nums[r]-(long)t) <= (long)nums[r] + (long)t)  
+                return true;
+        }
+        return false;
+    }
+}
