d608943b588ad2c8e3dd9389e32ae0be96395e18
tichen47 <tichen47@gmail.com>
2019-05-10 04:09:25 -0500

    Re-do problem 2, try serval times

--- src/linked_list/Add_Two_Numbers_2.java
+++ src/linked_list/Add_Two_Numbers_2.java
@@ -7,44 +7,32 @@ public class Add_Two_Numbers_2 {
      * Time Complexity: O(N)
      * Space Complexity: O(N)
      */
-    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        if (l1 == null)
-            return l2;
-        if (l2 == null)
-            return l1;
-
-        boolean addition = false;
-        ListNode ret = new ListNode(0);
-        ListNode head = ret;
-        ListNode temp;
-        while (true) {
-            if (l1 == null && l2 == null) {
-                if (addition) {
-                    head.next = new ListNode(1);
-                }
-                break;
-            } else if (l1 == null)
-                l1 = new ListNode(0);
-            else if (l2 == null)
-                l2 = new ListNode(0);
-
-            int sum = l1.val + l2.val;
-            if (addition)
-                sum++;
-
-            temp = new ListNode(sum % 10);
-            addition = false;
-
-            if (sum >= 10)
-                addition = true;
-
-            head.next = temp;
-            head = head.next;
-            l1 = l1.next;
-            l2 = l2.next;
+    public ListNode addTwoNumbers2(ListNode l1, ListNode l2) {
+        ListNode dummy = new ListNode(0);
+        ListNode ret = dummy;
+        
+        int flag = 0;
+        while(l1 != null || l2 != null){
+            int val1 = l1 == null ? 0 : l1.val;
+            int val2 = l2 == null ? 0 : l2.val;
+            int sum = val1 + val2 + flag;
+            dummy.next = new ListNode(sum % 10);
+            flag = sum >= 10 ? 1 : 0;
+            if(l1 != null) l1 = l1.next;
+            if(l2 != null) l2 = l2.next;
+            dummy = dummy.next;
         }
-
+        if(flag == 1) dummy.next = new ListNode(flag);
         return ret.next;
     }
-
+    
+    public static void main(String[] args) {
+        int[] t1 = new int[] {1, 8};
+        int[] t2 = new int[] {0};
+        ListNode l1 = new ListNode(t1);
+        ListNode l2 = new ListNode(t2);
+        Add_Two_Numbers_2 a = new Add_Two_Numbers_2();
+        ListNode ret = a.addTwoNumbers2(l1, l2);
+        System.out.println(ret);
+    }
 }
