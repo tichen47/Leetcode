46cfcdc24749e4b7ee4100d5f26dfc14b40d1c45
tichen47 <tichen47@gmail.com>
2019-05-10 04:23:18 -0500

    Redo problem 83, add recursive solution

--- src/linked_list/Remove_Duplicates_from_Sorted_List_83.java
+++ src/linked_list/Remove_Duplicates_from_Sorted_List_83.java
@@ -4,27 +4,26 @@ public class Remove_Duplicates_from_Sorted_List_83 {
 
     
     /*
-     * Solution: Faster than the next solution 
+     * Solution: Two Pointer (Faster than solution 2)
      * Time Complexity: O(n)
      * Space Complexity: O(1)
      */
     public ListNode deleteDuplicates(ListNode head) {
-        if(head == null)
-            return null;
-        ListNode ret = head;
-        ListNode temp = head;
-        
-        while(head != null){
-            if(head.val != temp.val){
-                temp.next = head;
-                temp = temp.next;
+        if(head == null) return null;
+        ListNode walk = head;
+        ListNode run = head;
+        while(run != null){
+            if(walk.val != run.val){
+                walk.next = run;
+                walk = walk.next;
             }
-            head = head.next;
+            run = run.next;
         }
-        temp.next = null;
-        return ret;
+        walk.next = null;
+        return head;
     }
     
+
     /*
      * Solution: Straight Forward 
      * Time Complexity: O(n)
@@ -41,5 +40,15 @@ public class Remove_Duplicates_from_Sorted_List_83 {
         }
         return head;
     }
-    
+
+    /*
+     * Solution: Recursive
+     * Time Complexity: O(n)
+     * Space Complexity: O(1)
+     */
+    public ListNode deleteDuplicates3(ListNode head) {
+        if(head == null || head.next == null) return head;
+        head.next = deleteDuplicates(head.next);
+        return head.val == head.next.val ? head.next : head;
+    }
 }
