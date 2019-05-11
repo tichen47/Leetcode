76ef83a9f6f9e3b2281f72731fd778d0c2b53231
tichen47 <tichen47@gmail.com>
2019-05-11 18:54:42 -0500

    Redo 41, try serval times for debugging

--- src/array/First_Missing_Positive_41.java
+++ src/array/First_Missing_Positive_41.java
@@ -1,49 +1,45 @@
 package array;
 
 public class First_Missing_Positive_41 {
-    
+
     /*
-     * Solution 1:
-     * Time Complexity: O(N)
+     * Solution 1: 
+     * Time Complexity: O(N) 
      * Space Complexity: O(1)
      */
     public int firstMissingPositive(int[] nums) {
         int len = nums.length;
-        
-        // Check if 1 is in the array
-        // Convert non-positive number, and number > n to 1
-        int numOfOne = 0;
-        for(int i = 0; i < len; i++){
-            if(nums[i] == 1)
-                numOfOne++;
-            else if(nums[i] < 1 || nums[i] > len)
+
+        // 1. Check if 1 is in the array
+        // 2. Convert negative number or number > length to 1;
+        boolean hasOne = false;
+        for (int i = 0; i < len; i++) {
+            if (nums[i] == 1)
+                hasOne = true;
+            if (nums[i] < 1 || nums[i] > len)
                 nums[i] = 1;
         }
-        
-        // Return if 1 is not in the array
-        if(numOfOne == 0)
+        if (!hasOne)
             return 1;
-        
-        // Go through the array
-        // If number k is in the array, change nums[k] to negative
-        int temp;
-        for(int i = 0; i < len; i++){
-            temp = Math.abs(nums[i]);
-            if(nums[temp-1] > 0)
-                nums[temp-1] = - Math.abs(nums[temp-1]);
+
+        // 3. For each element k in nums, make nums[k-1] negative
+        for (int i = 0; i < len; i++) {
+            int index = Math.abs(nums[i]) - 1;
+            nums[index] = -Math.abs(nums[index]);
         }
-        
-        // Find the first positive index
-        for(int i = 0; i < len; i++){
-            if(nums[i] > 0)
-                return i+1;
+
+        // 4. Check for positive index
+        for (int i = 1; i < len; i++) {
+            if (nums[i] > 0)
+                return i + 1;
         }
-        return len+1;
+
+        return len + 1;
     }
-    
-    /* 
-     * Solution 2: Hashset
-     * Time Complexity: O(N)
+
+    /*
+     * Solution 2: Hashset 
+     * Time Complexity: O(N) 
      * Space Complexity: O(N)
-     */ 
+     */
 }
