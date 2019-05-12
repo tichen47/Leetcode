988dc15378d9a8a5becc22db0141f4b61f6f416c
tichen47 <tichen47@gmail.com>
2019-05-11 19:06:36 -0500

    AC 53

--- src/dynamic_programming/Maximum_Subarray_53.java
+++ src/dynamic_programming/Maximum_Subarray_53.java
@@ -1,18 +1,18 @@
 package dynamic_programming;
 
 public class Maximum_Subarray_53 {
-    
+
     /*
-     * Solution:
-     * Time Complexity: O(N)
+     * Solution: DP
+     * Time Complexity: O(N) 
      * Space Complexity: O(1)
      */
     public int maxSubArray(int[] nums) {
-        if(nums == null) return 0;
-        int max = nums[0];
-        for(int i = 1; i < nums.length; i++){
-            if(nums[i-1] > 0)
-                nums[i] += nums[i-1];
+        int max = Integer.MIN_VALUE;
+
+        for (int i = 0; i < nums.length; i++) {
+            if (i != 0 && nums[i - 1] > 0)
+                nums[i] += nums[i - 1];
             max = Math.max(max, nums[i]);
         }
         return max;
