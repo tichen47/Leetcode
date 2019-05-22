e412d885d52006e48e1436742960d235849a4e5f
tichen47 <tichen47@gmail.com>
2019-05-22 01:47:19 -0500

    Problem 108

--- src/tree/Convert_Sorted_Array_to_Binary_Search_Tree_108.java
+++ src/tree/Convert_Sorted_Array_to_Binary_Search_Tree_108.java
@@ -7,13 +7,15 @@ public class Convert_Sorted_Array_to_Binary_Search_Tree_108 {
      * Space Complexity: O(N)
      */
     public TreeNode sortedArrayToBST(int[] nums) {
-        if(nums == null) return null;
-        return helper(nums, 0, nums.length-1);
+        if (nums == null)
+            return null;
+        return helper(nums, 0, nums.length - 1);
     }
-    
-    public TreeNode helper(int[] nums, int left, int right){
-        if(left > right) return null;
-        
+
+    public TreeNode helper(int[] nums, int left, int right) {
+        if (left > right)
+            return null;
+
         int mid = left + (right - left) / 2;
         TreeNode node = new TreeNode(nums[mid]);
         node.left = helper(nums, left, mid - 1);
