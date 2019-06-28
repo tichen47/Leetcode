d73348772cfd247740969d67196c501acb94eb39
tichen47 <tichen47@gmail.com>
2019-06-27 23:21:58 -0500

    Problem 48

---
+++ src/array/Rotate_Image_48.java
@@ -0,0 +1,23 @@
+package array;
+
+public class Rotate_Image_48 {    
+    /*
+     * Solution 1:
+     * Time Complexity: O(N^2)
+     * Space Complexity: O(1)
+     */
+    public void rotate(int[][] matrix) {
+        int len = matrix.length;
+
+        for(int i = 0; i < len / 2; i++){
+            for(int j = 0; j < (len + 1) / 2; j++){
+                
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[len-1-j][i];
+                matrix[len-1-j][i] = matrix[len-1-i][len-1-j];
+                matrix[len-1-i][len-1-j] = matrix[j][len-1-i];
+                matrix[j][len-1-i] = temp;
+            }
+        }
+    }
+}
