95931dd1ccf1e64b5fee0dcf2047139da7841b7f
tichen47 <tichen47@gmail.com>
2019-07-08 11:20:32 +0800

    Problem 34

--- src/search/Find_First_and_Last_Position_of_Element_in_Sorted_Array_34.java
+++ src/search/Find_First_and_Last_Position_of_Element_in_Sorted_Array_34.java
@@ -2,31 +2,66 @@ package search;
 
 public class Find_First_and_Last_Position_of_Element_in_Sorted_Array_34 {
     /*
-     * Solution: Binary Search
-     * Time Complexity: O(logN) 
+     * Solution: Fake Binary Search
+     * Time Complexity: O(N)
      * Space Complexity: O(1)
      */
     public int[] searchRange(int[] nums, int target) {
-        // Find target in nums[i, j]
-        if(nums.length < 1) return new int[]{-1, -1};
-        
-        int len = nums.length;
-        int left = 0, right = len - 1;
+        int left = 0, right = nums.length - 1;
+        boolean find = false;
         while(left <= right){
-            int mid = (left + right) / 2;
-            if(nums[mid] == target) break;
-            else if(nums[mid] > target)
-                right = mid - 1;
-            else
-                left = mid + 1;
+            int mid = left + (right - left) / 2;
+            if(nums[mid] == target){
+                find = true;
+                left = mid;
+                right = mid;
+                while(left > 0 && nums[left-1] == target) left--;
+                while(right < nums.length - 1 && nums[right+1] == target) right++;
+                break;
+            }
+            else if(nums[mid] > target) right = mid - 1;
+            else left = mid + 1;
         }
-        int pos = (left + right) / 2;
-        if(nums[pos] != target) return new int[]{-1, -1};
-        else{
-            int start = pos, end = pos;
-            while(start > 0 && nums[start-1] == target) start--;
-            while(end < len - 1 && nums[end+1] == target) end++;
-            return new int[]{start, end};
+        return find ? new int[]{left, right} : new int[]{-1, -1};
+    }
+    
+    /*
+     * Solution: Real Binary Search
+     * Time Complexity: O(logN)
+     * Space Complexity: O(1)
+     */
+    public int[] searchRange2(int[] nums, int target) {
+        int left = findFirst(nums, target);
+        int right = findLast(nums, target, left);
+        return new int[]{left, right};
+    }
+    
+    public int findFirst(int[] nums, int target){
+        int idx = -1;
+        int left = 0, right = nums.length-1;
+        
+        while(left <= right){
+            int mid = left + (right - left) / 2;
+            if(nums[mid] >= target) right = mid - 1;
+            else left = mid + 1;
+            
+            if(nums[mid] == target) idx = mid;
         }
+        return idx;
+    }
+    
+    public int findLast(int[] nums, int target, int start){
+        int idx = -1;
+        int left = start, right = nums.length-1;
+        if(left == -1) return -1;
+        
+        while(left <= right){
+            int mid = left + (right - left) / 2;
+            if(nums[mid] <= target) left = mid + 1;
+            else right = mid - 1;
+            
+            if(nums[mid] == target) idx = mid;
+        }
+        return idx;
     }
 }
