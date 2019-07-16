96eb0d2a5a487a77254079bddfdd24e7a1de49da
tichen47 <tichen47@gmail.com>
2019-07-16 14:47:07 +0800

    Problem 24 redo, done

--- src/linked_list/Swap_Nodes_in_Pairs_24.java
+++ src/linked_list/Swap_Nodes_in_Pairs_24.java
@@ -8,16 +8,20 @@ public class Swap_Nodes_in_Pairs_24 {
      * Space Complexity: O(1)
      */
     public ListNode swapPairs(ListNode head) {
+        if(head == null) return head;
         ListNode dummy = new ListNode(0);
         dummy.next = head;
+        
         ListNode pre = dummy;
-        ListNode cur = pre.next;
-        while (cur != null && cur.next != null) {
-            pre.next = cur.next;
-            cur.next = cur.next.next;
-            pre.next.next = cur;
-            pre = pre.next.next;
-            cur = cur.next;
+        while(pre.next != null && pre.next.next != null){
+            // Ex: 0(pre)->1->2->3->4  
+            // Goal: Swap 1 and 2
+            ListNode cur = pre.next;   // pre: 0, cur: 1
+            pre.next = cur.next;       // 0->2  
+            cur.next = cur.next.next;  // 1->3  
+            pre.next.next = cur;       // 2->1  (Achieve: 0(pre)->2->1->3->4)
+            
+            pre = pre.next.next;       // (0(->2->1(pre)->3->4)
         }
         return dummy.next;
     }
