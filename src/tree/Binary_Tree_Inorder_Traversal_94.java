8b32cb81b649ce514ce02035126a1604de18519a
tichen47 <tichen47@gmail.com>
2019-07-25 22:58:07 +0800

    Problem 94 redo, done

--- src/tree/Binary_Tree_Inorder_Traversal_94.java
+++ src/tree/Binary_Tree_Inorder_Traversal_94.java
@@ -45,4 +45,25 @@ public class Binary_Tree_Inorder_Traversal_94 {
         }
         return ret;
     }
+    
+    /*
+     * Solution 3: Iterative 
+     * Time Complexity: O(N) 
+     * Space Complexity: O(N)
+     */
+    public List<Integer> inorderTraversal3(TreeNode root) {
+        List<Integer> res = new ArrayList<>();
+        Deque<TreeNode> stack = new LinkedList<>();
+        
+        while(!stack.isEmpty() || root != null){
+            while(root != null){
+                stack.push(root);
+                root = root.left;
+            }
+            root = stack.pop();
+            res.add(root.val);
+            root = root.right;
+        }
+        return res;
+    }
 }
