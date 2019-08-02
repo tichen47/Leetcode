c0a04047a9cbfa1805dc7b1b39bc28dd2e4123d8
tichen47 <tichen47@gmail.com>
2019-08-03 01:07:42 +0800

    Problem 3 redo, add second solution

--- src/sliding_window/Length_Of_Longest_Substring_3.java
+++ src/sliding_window/Length_Of_Longest_Substring_3.java
@@ -24,4 +24,22 @@ public class Length_Of_Longest_Substring_3 {
         }
         return max;
     }
+    
+    /*
+     * Solution: Sliding Window with map
+     * Time Complexity: O(N)
+     * Space Complexity: O(1)
+     */
+    public int lengthOfLongestSubstring2(String s) {
+        int n = s.length(), ans = 0;
+        Map<Character, Integer> map = new HashMap<>();
+        for(int i = 0, j = 0; j < n; j++){
+            char ch = s.charAt(j);
+            if(map.containsKey(ch))
+                i = Math.max(i, map.get(ch) + 1);   // Math.max used for: "abba"
+            ans = Math.max(ans, j - i + 1);
+            map.put(ch, j);
+        }
+        return ans;
+    }
 }
