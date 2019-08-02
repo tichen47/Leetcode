e99983db955ab913d477adf249fd3256641592bb
tichen47 <tichen47@gmail.com>
2019-08-03 00:36:15 +0800

    Problem 6 redo, add second solution

--- src/string/ZigZag_Conversion_6.java
+++ src/string/ZigZag_Conversion_6.java
@@ -1,6 +1,11 @@
 package string;
 
 public class ZigZag_Conversion_6 {
+    /*
+     * Solution: Sort by Row
+     * Time Complexity: O(N) 
+     * Space Complexity: O(N)
+     */ 
     public String convert(String s, int numRows) {
         char[] arr = s.toCharArray();
         int n = arr.length;
@@ -20,5 +25,27 @@ public class ZigZag_Conversion_6 {
         for(int i = 0; i < sbs.length; i++)
             ret += sbs[i].toString();
         return ret;
+    }
+    
+    /*
+     * Solution: Visit by Row
+     * Time Complexity: O(N) 
+     * Space Complexity: O(N)
+     */ 
+    public String convert2(String s, int numRows) {
+        if(numRows == 1) return s;
+        
+        StringBuilder sb = new StringBuilder();
+        int n = s.length();
+        int diff = 2 * numRows - 2;  // diff = 0 if numRows = 1
+        
+        for(int i = 0; i < numRows; i++){
+            for(int j = 0; j + i < n; j += diff){ // j : num of cycle
+                sb.append(s.charAt(j + i));
+                if(i != 0 && i != numRows - 1 && j + diff - i < n)
+                    sb.append(s.charAt(j + diff - i));
+            }
+        }
+        return sb.toString();
     }
 }
