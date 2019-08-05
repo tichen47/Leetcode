43e3583d8c9ee23d17d4d5bc1f716e3199fc92f5
tichen47 <tichen47@gmail.com>
2019-08-05 23:45:48 +0800

    Problem 17 redo

--- src/backtracking/Letter_Combinations_of_a_Phone_Number_17.java
+++ src/backtracking/Letter_Combinations_of_a_Phone_Number_17.java
@@ -29,4 +29,51 @@ public class Letter_Combinations_of_a_Phone_Number_17 {
             helper(ret, path + letter.charAt(i), index+1, digits);
         }
     }
+    
+    /*
+     * Solution: Iteration
+     * Time Complexity: O(4^N) 
+     * Space Complexity: O(4^N)
+     */  
+    public List<String> letterCombinations2(String digits) {
+        String[] board = new String[]{"0", "1", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
+        LinkedList<String> res = new LinkedList<>();
+        if(digits.length() == 0) return res;
+        
+        res.add("");
+        for(int i = 0; i < digits.length(); i++){
+            int num = digits.charAt(i) - '0';
+            String choice = board[num];
+            int it = res.size();
+            for(int j = 0; j < it; j++){
+                String t = res.remove();
+                for(int k = 0; k < choice.length(); k++)
+                    res.add(t + choice.charAt(k));
+            }
+        }
+        return res;
+    }
+    
+    /*
+     * Solution: Simplify of previous solution
+     * Time Complexity: O(4^N) 
+     * Space Complexity: O(4^N)
+     */  
+    public List<String> letterCombinations3(String digits) {
+        String[] board = new String[]{"0", "1", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
+        LinkedList<String> res = new LinkedList<>();
+        int len = digits.length();
+        if(len == 0) return res;
+        res.add("");
+        
+        while(res.peek().length() != len){
+            String next = res.remove();
+            int index = digits.charAt(next.length()) - '0';
+            char[] choice = board[index].toCharArray();
+            for(char ch : choice)
+                res.add(next + ch);
+        }
+        
+        return res;
+    }
 }
