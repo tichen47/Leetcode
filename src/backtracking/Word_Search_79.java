f3b33992730e1cd4f6d2caa01f870ae4bd1544e4
tichen47 <tichen47@gmail.com>
2019-08-07 22:56:54 +0800

    Problem 79 redo

--- src/backtracking/Word_Search_79.java
+++ src/backtracking/Word_Search_79.java
@@ -6,55 +6,34 @@ public class Word_Search_79 {
      * Time Complexity:  O(M * N * (4^L)) : M*N is the size of the board, 4^L for each cell
      * Space Complexity: O(L): L is the length of the word
      */ 
-    boolean[][] visited;
-
+    char[][] board;
+    boolean[][] used;
+    
     public boolean exist(char[][] board, String word) {
-        if (word == null || word.length() == 0)
-            return true;
-        if (board == null)
-            return false;
-
-        visited = new boolean[board.length][board[0].length];
-        for (int i = 0; i < board.length; i++) {
-            for (int j = 0; j < board[0].length; j++) {
-                if (word.charAt(0) == board[i][j] && exist(board, word, 0, i, j))
-                    return true;
-            }
+        this.board = board;
+        int m = board.length;
+        int n = board[0].length;
+        used = new boolean[m][n];
+        
+        for(int i = 0; i < m; i++){
+            for(int j = 0; j < n; j++)
+                if(expandCenter(word, 0, i, j, m, n)) return true;
         }
         return false;
     }
-
-    public boolean exist(char[][] board, String word, int start, int x, int y) {
-        // Finish compare
-        if (start == word.length())
-            return true;
-
-        // Out of bound
-        if (x < 0 || x >= board.length || y < 0 || y >= board[0].length)
-            return false;
-
-        // Already in the set
-        if (visited[x][y])
-            return false;
-
-        // Value not equal
-        if (word.charAt(start) != board[x][y])
-            return false;
-
-        visited[x][y] = true;
-
-        if (exist(board, word, start + 1, x - 1, y) || exist(board, word, start + 1, x, y + 1)
-                || exist(board, word, start + 1, x + 1, y) || exist(board, word, start + 1, x, y - 1))
-            return true;
-
-        visited[x][y] = false;
-        return false;
-    }
-
-    public static void main(String[] args) {
-        char[][] board = new char[][] { { 'a', 'b' } };
-        String word = "ba";
-        Word_Search_79 w = new Word_Search_79();
-        System.out.println(w.exist(board, word));
+    
+    public boolean expandCenter(String word, int index, int i, int j, int m, int n) {
+        if(index >= word.length()) return true;
+        if(i < 0 || i >= m || j < 0 || j >= n || used[i][j] || word.charAt(index) != board[i][j]) return false;
+        
+        used[i][j] = true;
+        
+        boolean res = expandCenter(word, index+1, i+1, j, m, n)
+            || expandCenter(word, index+1, i, j+1, m, n)
+            || expandCenter(word, index+1, i-1, j, m, n)
+            || expandCenter(word, index+1, i, j-1, m, n);
+        
+        used[i][j] = false;
+        return res;
     }
 }
