9809f25f6222a906a4bab04a3cd11eab6d1fd126
tichen47 <tichen47@gmail.com>
2019-08-19 17:11:41 -0500

    Problem 78 redo, need more practice

--- src/backtracking/Subsets_78.java
+++ src/backtracking/Subsets_78.java
@@ -8,24 +8,19 @@ public class Subsets_78 {
      * Time Complexity:  O(2^N): total 2^n possible subsets can be there for n items
      * Space Complexity: O(N+N): maximum depth of recursion tree + the list space
      */ 
-    List<List<Integer>> ret;
-    List<Integer> path;
-    
     public List<List<Integer>> subsets(int[] nums) {
-        ret = new ArrayList<>();
-        path = new ArrayList<>();
-        
-        subsets(nums, 0, nums.length);
-        return ret;
+        List<List<Integer>> res = new ArrayList<>();
+        dfs(res, new ArrayList<Integer>(), 0, nums);
+        return res;
     }
     
-    public void subsets(int[] nums, int index, int len){
-        ret.add(new ArrayList<>(path));
+    public void dfs(List<List<Integer>> res, List<Integer> cur, int start, int[] nums){
+        res.add(new ArrayList<>(cur));
         
-        for(int i = index; i < len; i++){
-            path.add(nums[i]);
-            subsets(nums, i+1, len);
-            path.remove(path.size()-1);
+        for(int i = start; i < nums.length; i++) {
+            cur.add(nums[i]);
+            dfs(res, cur, i + 1, nums);
+            cur.remove(cur.size() - 1);
         }
     }
 }
