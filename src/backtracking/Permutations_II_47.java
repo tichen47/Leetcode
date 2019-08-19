de1961514d617ad1400da5fb133763f020f3a218
tichen47 <tichen47@gmail.com>
2019-08-19 16:30:24 -0500

    Problem 47 redo, need more practice

--- src/backtracking/Permutations_II_47.java
+++ src/backtracking/Permutations_II_47.java
@@ -8,21 +8,18 @@ public class Permutations_II_47 {
      * Time Complexity: O(N*2^N) 
      * Space Complexity: ???
      */ 
-    List<List<Integer>> ret;
-    List<Integer> numLs;
-    
     public List<List<Integer>> permuteUnique(int[] nums) {
-        ret = new ArrayList<>();
-        numLs = new ArrayList<>();
         Arrays.sort(nums);
+        List<List<Integer>> res = new ArrayList<>();
+        List<Integer> numLs = new ArrayList<>();
         for(int num : nums) numLs.add(num);
-        permuteUnique(0, nums.length);
-        return ret;
+        dfs(0, res, numLs, nums.length);
+        return res;
     }
     
-    public void permuteUnique(int start, int len){
+    public void dfs(int start, List<List<Integer>> res, List<Integer> numLs, int len) {
         if(start == len - 1){
-            ret.add(new ArrayList<Integer>(numLs));
+            res.add(new ArrayList<>(numLs));
             return;
         }
         
@@ -30,14 +27,14 @@ public class Permutations_II_47 {
             // Skip duplicate
             if(i > start && numLs.get(i) == numLs.get(i-1)) continue;
             
-            // move current number to the start position
+            // move current number to the start position (not swap!)
             numLs.add(start, numLs.get(i));
-            numLs.remove(i+1);    
+            numLs.remove(i+1);
             
-            permuteUnique(start+1, len);
+            dfs(start+1, res, numLs, len);
             
             // move current number back to origin position
-            numLs.add(i + 1, numLs.get(start));
+            numLs.add(i+1, numLs.get(start));
             numLs.remove(start);
         }
     }
