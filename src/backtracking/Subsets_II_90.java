522d49734a097a3c3072bd5d45406b3622286f7f
tichen47 <tichen47@gmail.com>
2019-08-20 15:39:41 -0500

    Problem 90 redo, done

--- src/backtracking/Subsets_II_90.java
+++ src/backtracking/Subsets_II_90.java
@@ -4,9 +4,9 @@ import java.util.*;
 
 public class Subsets_II_90 {
     /*
-     * Solution: Backtracking
+     * Solution: Backtracking DFS
      * Time Complexity:  O(2^N): total 2^n possible subsets can be there for n items
-     * Space Complexity: O(N+N): maximum depth of recursion tree + the list space
+     * Space Complexity: O(2^N)
      */ 
     List<List<Integer>> ret;
     List<Integer> path;
@@ -28,5 +28,33 @@ public class Subsets_II_90 {
             subsetsWithDup(nums, i+1, len);
             path.remove(path.size()-1);
         }
+    }
+    
+    /*
+     * Solution: BFS
+     * Time Complexity:  O(2^N): total 2^n possible subsets can be there for n items
+     * Space Complexity: O(2^N) 
+     */ 
+    public List<List<Integer>> subsetsWithDup2(int[] nums) {
+        Arrays.sort(nums);
+        List<List<Integer>> res = new ArrayList<>();
+        res.add(new ArrayList<>());
+        
+        int begin = 0; // If the nums[i] equals to nums[i-1], nums[i] will only add to subsets which contain nums[i-1]
+                       // res[begin, len) are all the subsets which contains nums[i-1]  
+                       // If nums[i] does not equal to nums[i-1], nums[i] will add to all subsets -> begin = 0
+        
+        for(int i = 0; i < nums.length; i++){
+            if(i != 0 && nums[i] != nums[i-1]) begin = 0;
+            int len = res.size();
+            for(int j = begin; j < len; j++){
+                List<Integer> tempLs = new ArrayList<>(res.get(j));
+                tempLs.add(nums[i]);
+                res.add(tempLs);
+            }
+            begin = len;
+        }
+        
+        return res;
     }
 }
