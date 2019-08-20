876509e22bcc8f95a08dffff510300efbc7c2679
tichen47 <tichen47@gmail.com>
2019-08-20 18:37:57 -0500

    Problem 216 redo, done

--- src/backtracking/Combination_Sum_III_216.java
+++ src/backtracking/Combination_Sum_III_216.java
@@ -8,26 +8,22 @@ public class Combination_Sum_III_216 {
      * Time Complexity: O(9^k)
      * Space Complexity: O(k) 
      */ 
-    List<List<Integer>> ret;
-    List<Integer> cur;
-    
     public List<List<Integer>> combinationSum3(int k, int n) {
-        ret = new ArrayList<>();
-        cur = new ArrayList<>();
-        
-        helper(k, n, 1);
-        return ret;
+        List<List<Integer>> res = new ArrayList<>();
+        List<Integer> cur = new ArrayList<>();
+        dfs(res, cur, k, n, 1);
+        return res;
     }
     
-    public void helper(int k, int n, int index){
+    public void dfs(List<List<Integer>> res, List<Integer> cur, int k, int n, int min){
         if(k == 0){
-            if(n == 0) ret.add(new ArrayList<>(cur));
+            if(n == 0) res.add(new ArrayList<>(cur));
             return;
         }
         
-        for(int i = index; i <= 9 && i <= n; i++){
+        for(int i = min; i <= 9; i++){
             cur.add(i);
-            helper(k-1, n-i, i+1);
+            dfs(res, cur, k-1, n-i, i+1);
             cur.remove(cur.size()-1);
         }
     }
