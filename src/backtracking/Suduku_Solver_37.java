f81ebb7cd7e486519404a4a037c44c222ef02776
tichen47 <tichen47@gmail.com>
2019-08-21 17:38:39 -0500

    Redo 37, need more practice

--- src/backtracking/Suduku_Solver_37.java
+++ src/backtracking/Suduku_Solver_37.java
@@ -8,37 +8,34 @@ public class Suduku_Solver_37 {
      * Space Complexity: O(N): Recursive space
      */
     public void solveSudoku(char[][] board) {
-        solve(board);
+        dfs(board, 0, 0);
     }
-
-    public boolean solve(char[][] board) {
-        for (int i = 0; i < 9; i++) {
-            for (int j = 0; j < 9; j++) {
-                // Once find next empty cell
-                if (board[i][j] == '.') {
-                    for (char value = '1'; value <= '9'; value++) {
-                        if (valid(board, i, j, value)) {
-                            board[i][j] = value;
-
-                            if (solve(board))
-                                return true;
-
-                            board[i][j] = '.';
-                        }
-                    }
-                    return false;
+    
+    public boolean dfs(char[][] board, int row, int col) {
+        for(int i = row; i < 9; i++, col = 0){
+            for(int j = col; j < 9; j++){
+                if(board[i][j] != '.') continue;
+                
+                for(char c = '1'; c <= '9'; c++){
+                    if(isValid(board, i, j, c)){
+                        board[i][j] = c;
+                        if(dfs(board, i, j + 1)) return true; // Find answer
+                        board[i][j] = '.';
+                    }    
                 }
+                return false;
+                
             }
         }
         return true;
     }
-
-    public boolean valid(char[][] board, int row, int col, char value) {
-        int blockRow = row / 3 * 3;
-        int blockCol = col / 3 * 3;
-        for (int i = 0; i < 9; i++) {
-            if (board[i][col] == value || board[row][i] == value || board[blockRow + i / 3][blockCol + i % 3] == value)
-                return false;
+    
+    public boolean isValid(char[][] board, int row, int col, char c){
+        int rowBox = row / 3 * 3;
+        int colBox = col / 3 * 3;
+        for(int i = 0; i < 9; i++){
+            if(board[row][i] == c || board[i][col] == c
+              || board[rowBox + i / 3][colBox + i % 3] == c) return false;
         }
         return true;
     }
