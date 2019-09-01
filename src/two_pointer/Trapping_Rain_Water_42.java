67974499a7779c2a39b7262c82910f98290c0db1
Ti Chen <tichen47@gmail.com>
2019-09-01 15:16:12 -0500

    Redo Problem 42 with two pointer

--- src/two_pointer/Trapping_Rain_Water_42.java
+++ src/two_pointer/Trapping_Rain_Water_42.java
@@ -53,17 +53,21 @@ public class Trapping_Rain_Water_42 {
      * Space Complexity: O(1)
      */
     public int trap3(int[] height) {
-        int res = 0;
-        int max_left = 0, max_right = 0;
         int left = 0, right = height.length - 1;
+        int leftMax = 0, rightMax = 0;
+        int res = 0;
         while(left < right) {
             if(height[left] < height[right]) {
-                if(height[left] > max_left) max_left = height[left];
-                else res += max_left - height[left];
+                if(height[left] > leftMax)
+                    leftMax = height[left];
+                else
+                    res += leftMax - height[left];
                 left++;
             } else {
-                if(height[right] > max_right) max_right = height[right];
-                else res += max_right - height[right];
+                if(height[right] > rightMax)
+                    rightMax = height[right];
+                else
+                    res += rightMax - height[right];
                 right--;
             }
         }
