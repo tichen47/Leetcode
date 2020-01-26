32316d13231b28f4d0a395fa072afb499eee9b9e
tichen47 <tichen47@gmail.com>
2020-01-26 04:50:38 -0600

    89

---
+++ src/backtracking/Gray_Code_89.java
@@ -0,0 +1,14 @@
+package backtracking;
+
+public class Gray_Code_89 {
+    public List<Integer> grayCode(int n) {
+        List<Integer> res = new ArrayList<>();
+        res.add(0);
+        for(int i = 0; i < n; i++) {
+            int base = 1 << i;
+            for(int j = res.size() - 1; j >= 0; j--)
+                res.add(res.get(j) + base);
+        }
+        return res;
+    }
+}
